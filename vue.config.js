363d6cb8b2243f0b8b841ac2ef0d1b281b30df36
zjk <2455650395@qq.com>
2023-05-20 13:59:22 +0800

    网站上线

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: "./", //配置本地引用目录文件,如不配置，默认为`/`
+});
